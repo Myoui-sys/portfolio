99fbf503fe50c2c743e64d0267a636b55e4891c2
Dacyrrôse Melo <74743936+Myoui-sys@users.noreply.github.com>
2026-09-03 15:23:43 -0300

    Arquitetura do portfólio

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Skills from "./pages/Skills/Skills";
+import Projects from "./pages/Projects/Projects";
+import Experience from "./pages/Experience/Experience";
+import Contact from "./pages/Contact/Contact";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/habilidades" element={<Skills />} />
+      <Route path="/projetos" element={<Projects />} />
+      <Route path="/experiencia" element={<Experience />} />
+      <Route path="/contato" element={<Contact />} />
+    </Routes>
+  );
+}
+
+export default App;
